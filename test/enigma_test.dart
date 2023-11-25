a1df6ab36f6fead57a212c5308c68744a8dbb1ad
Carlos Makin <carlos@makin.dev>
2023-11-24 23:10:12 -0700

    Update enigma_test.dart

--- test/enigma_test.dart
+++ test/enigma_test.dart
@@ -5,109 +5,99 @@ import 'package:enigma/enigma.dart';
 import 'package:test/test.dart';
 
 void main() {
+  final Uint8List iv = generateRandomIV();
+
   group('enigma keygen', () {
     test('generate random IV', () {
-      final Uint8List iv = generateRandomIV();
       expect(iv.length, equals(16));
     });
 
-    test('generate random key', () {
-      for (final AESKeyStrength strength in AESKeyStrength.values) {
+    for (final AESKeyStrength strength in AESKeyStrength.values) {
+      test('generate random ${strength.numBits}-bit key', () {
         final Uint8List key = generateRandomKey(strength);
         expect(key.length, equals(strength.numBytes));
-      }
-    });
+      });
+    }
 
-    test('derive key from passphrase', () {
-      for (final AESKeyStrength strength in AESKeyStrength.values) {
+    for (final AESKeyStrength strength in AESKeyStrength.values) {
+      test('derive ${strength.numBits}-bit key from passphrase', () {
         final Uint8List key =
             deriveKeyFromPassphrase('password', iterations: 1, strength: strength);
         expect(key.length, equals(strength.numBytes));
-      }
-    });
+      });
+    }
 
-    test('derive key from passphrase with salt', () {
-      for (final AESKeyStrength strength in AESKeyStrength.values) {
+    for (final AESKeyStrength strength in AESKeyStrength.values) {
+      test('derive ${strength.numBits}-bit key from passphrase with salt', () {
         final Uint8List unsaltedKey =
             deriveKeyFromPassphrase('password', iterations: 1, strength: strength);
         final Uint8List saltedKey =
-            deriveKeyFromPassphrase('password', salt: 'bae', iterations: 1, strength: strength);
+            deriveKeyFromPassphrase('password', salt: 'salt', iterations: 1, strength: strength);
         expect(saltedKey.length, equals(strength.numBytes));
         expect(saltedKey, isNot(unsaltedKey));
-      }
-    });
+      });
+    }
 
-    test('key reproducibility', () {
-      for (final AESKeyStrength strength in AESKeyStrength.values) {
+    for (final AESKeyStrength strength in AESKeyStrength.values) {
+      test('${strength.numBits}-bit key reproducibility', () {
         final Uint8List key1 =
-            deriveKeyFromPassphrase('password', salt: 'bae', iterations: 1, strength: strength);
+            deriveKeyFromPassphrase('password', salt: 'salt', iterations: 1, strength: strength);
         final Uint8List key2 =
-            deriveKeyFromPassphrase('password', salt: 'bae', iterations: 1, strength: strength);
+            deriveKeyFromPassphrase('password', salt: 'salt', iterations: 1, strength: strength);
         expect(key1, key2);
-      }
-    });
+      });
+    }
   });
 
   group('enigma cryptography', () {
-    test('encrypt and decrypt text', () {
-      const String original = 'Hello world!';
+    const String text = 'Hello world!';
+    final Uint8List smallBytes = Uint8List.fromList(utf8.encode('data'));
+    final Uint8List largeBytes = Uint8List.fromList(List<int>.filled(1024 * 1024, 64));
 
-      final Uint8List iv = generateRandomIV();
+    for (final AESKeyStrength strength in AESKeyStrength.values) {
+      final Uint8List key = generateRandomKey(strength);
 
-      for (final AESKeyStrength strength in AESKeyStrength.values) {
-        final Uint8List key = generateRandomKey(strength);
-        final String encrypted = encryptText(key: key, iv: iv, text: original);
+      test('encrypt/decrypt text with ${strength.numBits}-bit key', () {
+        final String encrypted = encryptText(key: key, iv: iv, text: text);
         final String decrypted = decryptText(key: key, iv: iv, text: encrypted);
-        expect(decrypted, equals(original));
-      }
+        expect(decrypted, equals(text));
 
-      for (final AESKeyStrength strength in AESKeyStrength.values) {
-        final Uint8List key = generateRandomKey(strength);
-        final String encrypted = encryptTextWithEmbeddedIV(key: key, iv: iv, text: original);
-        final String decrypted = decryptTextWithEmbeddedIV(key: key, text: encrypted);
-        expect(decrypted, equals(original));
-      }
-    });
+        final String encryptedWithIV = encryptTextWithEmbeddedIV(key: key, iv: iv, text: text);
+        final String decryptedWithIV = decryptTextWithEmbeddedIV(key: key, text: encryptedWithIV);
+        expect(decryptedWithIV, equals(text));
+      });
+    }
 
-    test('encrypt and decrypt bytes', () {
-      final Uint8List original = Uint8List.fromList(utf8.encode('data'));
+    for (final AESKeyStrength strength in AESKeyStrength.values) {
+      final Uint8List key = generateRandomKey(strength);
 
-      final Uint8List iv = generateRandomIV();
-
-      for (final AESKeyStrength strength in AESKeyStrength.values) {
-        final Uint8List key = generateRandomKey(strength);
-        final Uint8List encrypted = encryptBytes(key: key, iv: iv, data: original);
+      test('encrypt/decrypt bytes with ${strength.numBits}-bit key', () {
+        final Uint8List encrypted = encryptBytes(key: key, iv: iv, data: smallBytes);
         final Uint8List decrypted = decryptBytes(key: key, iv: iv, data: encrypted);
-        expect(decrypted, equals(original));
-      }
+        expect(decrypted, equals(smallBytes));
 
-      for (final AESKeyStrength strength in AESKeyStrength.values) {
-        final Uint8List key = generateRandomKey(strength);
-        final Uint8List encrypted = encryptBytesWithEmbeddedIV(key: key, iv: iv, data: original);
-        final Uint8List decrypted = decryptBytesWithEmbeddedIV(key: key, data: encrypted);
-        expect(decrypted, equals(original));
-      }
-    });
+        final Uint8List encryptedWithIV =
+            encryptBytesWithEmbeddedIV(key: key, iv: iv, data: smallBytes);
+        final Uint8List decryptedWithIV =
+            decryptBytesWithEmbeddedIV(key: key, data: encryptedWithIV);
+        expect(decryptedWithIV, equals(smallBytes));
+      });
+    }
 
-    test('encrypt and decrypt bytes with isolates', () async {
-      final Uint8List original = Uint8List.fromList(List<int>.filled(1024 * 1024, 64));
+    for (final AESKeyStrength strength in AESKeyStrength.values) {
+      final Uint8List key = generateRandomKey(strength);
 
-      final Uint8List iv = generateRandomIV();
-
-      for (final AESKeyStrength strength in AESKeyStrength.values) {
-        final Uint8List key = generateRandomKey(strength);
-        final Uint8List encrypted = await encryptBytesFast(key: key, iv: iv, data: original);
+      test('encrypt/decrypt bytes with ${strength.numBits}-bit key concurrently', () async {
+        final Uint8List encrypted = await encryptBytesFast(key: key, iv: iv, data: largeBytes);
         final Uint8List decrypted = await decryptBytesFast(key: key, iv: iv, data: encrypted);
-        expect(decrypted, equals(original));
-      }
+        expect(decrypted, equals(largeBytes));
 
-      for (final AESKeyStrength strength in AESKeyStrength.values) {
-        final Uint8List key = generateRandomKey(strength);
-        final Uint8List encrypted =
-            await encryptBytesWithEmbeddedIVFast(key: key, iv: iv, data: original);
-        final Uint8List decrypted = await decryptBytesWithEmbeddedIVFast(key: key, data: encrypted);
-        expect(decrypted, equals(original));
-      }
-    });
+        final Uint8List encryptedWithIV =
+            await encryptBytesWithEmbeddedIVFast(key: key, iv: iv, data: largeBytes);
+        final Uint8List decryptedWithIV =
+            await decryptBytesWithEmbeddedIVFast(key: key, data: encryptedWithIV);
+        expect(decryptedWithIV, equals(largeBytes));
+      });
+    }
   });
 }
