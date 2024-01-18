6cebb67b15cb1d705f088411ae2b1241e5907960
Carlos Makin <carlos@makin.dev>
2024-01-18 14:21:31 -0700

    Update enigma.dart

--- lib/enigma.dart
+++ lib/enigma.dart
@@ -1,7 +1,13 @@
-/// This utility file contains  methods to:
-/// - Generate secure random cryptographic keys and initial vectors (IVs) using the Fortuna algorithm (industry-standard CSPRNG).
-/// - Derive keys from user-supplied passphrases using PBKDF2 with SHA-256 HMAC (industry-standard method for password-based key derivation).
-/// - Encrypt and decrypt data using AES encryption with Cipher Block Chaining (CBC) mode (widely-adopted symmetric encryption standard).
+/// The Enigma Dart package provides a comprehensive suite of cryptographic functionalities, including:
+/// - Generation of secure random cryptographic keys and initial vectors (IVs) using the Fortuna algorithm (industry-standard CSPRNG).
+/// - Derivation of keys from user-supplied passphrases using PBKDF2 with SHA-256 HMAC (industry-standard method for password-based key derivation).
+/// - Encryption and decryption of data using various methods including:
+///   - AES encryption in Cipher Block Chaining (CBC) mode, a widely-adopted symmetric encryption standard.
+///   - AES encryption in Galois/Counter Mode (GCM), offering authenticated encryption with integrated authentication.
+///   - ChaCha20 encryption as specified in RFC 7539, suitable for high-performance streaming data encryption.
+///   - ChaCha20-Poly1305 for authenticated encryption, combining confidentiality and data integrity.
+/// - Implementation of RSA cryptography, supporting key generation, encryption, decryption, signing, and verification with various key strengths.
+/// This package aims to offer robust, efficient, and easy-to-use cryptographic operations for Dart applications.
 library enigma;
 
 export 'src/aes.dart';
