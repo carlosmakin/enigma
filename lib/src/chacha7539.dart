e61a33b48f5c3df2832908f85f451b1dbf3f2f62
Carlos Makin <carlos@makin.dev>
2024-01-20 17:40:57 -0700

    Update chacha7539.dart

--- lib/src/chacha7539.dart
+++ lib/src/chacha7539.dart
@@ -12,9 +12,10 @@ void ensureLittleEndian(Uint32List list) {
 }
 
 /// Rotates the left bits of a 32-bit unsigned integer.
-int _rotateLeft32(int value, int shift) {
-  return ((value << shift)) | (value >> (32 - shift));
-}
+int rotateLeft32By16(int value) => (value << 16) | (value >> 16);
+int rotateLeft32By12(int value) => (value << 12) | (value >> 20);
+int rotateLeft32By8(int value) => (value << 8) | (value >> 24);
+int rotateLeft32By7(int value) => (value << 7) | (value >> 25);
 
 /// Performs the core rounds of the ChaCha20 block cipher.
 ///
@@ -30,86 +31,86 @@ void chacha20BlockRounds(Uint32List state) {
     // Column rounds
 
     // Quarter round on (0, 4, 8, 12)
-    state[0] = (state[0] + state[4]);
-    state[12] = _rotateLeft32(state[12] ^ state[0], 16);
-    state[8] = (state[8] + state[12]);
-    state[4] = _rotateLeft32(state[4] ^ state[8], 12);
-    state[0] = (state[0] + state[4]);
-    state[12] = _rotateLeft32(state[12] ^ state[0], 8);
-    state[8] = (state[8] + state[12]);
-    state[4] = _rotateLeft32(state[4] ^ state[8], 7);
+    state[0] = state[0] + state[4];
+    state[12] = rotateLeft32By16(state[12] ^ state[0]);
+    state[8] = state[8] + state[12];
+    state[4] = rotateLeft32By12(state[4] ^ state[8]);
+    state[0] = state[0] + state[4];
+    state[12] = rotateLeft32By8(state[12] ^ state[0]);
+    state[8] = state[8] + state[12];
+    state[4] = rotateLeft32By7(state[4] ^ state[8]);
 
     // Quarter round on (1, 5, 9, 13)
-    state[1] = (state[1] + state[5]);
-    state[13] = _rotateLeft32(state[13] ^ state[1], 16);
-    state[9] = (state[9] + state[13]);
-    state[5] = _rotateLeft32(state[5] ^ state[9], 12);
-    state[1] = (state[1] + state[5]);
-    state[13] = _rotateLeft32(state[13] ^ state[1], 8);
-    state[9] = (state[9] + state[13]);
-    state[5] = _rotateLeft32(state[5] ^ state[9], 7);
+    state[1] = state[1] + state[5];
+    state[13] = rotateLeft32By16(state[13] ^ state[1]);
+    state[9] = state[9] + state[13];
+    state[5] = rotateLeft32By12(state[5] ^ state[9]);
+    state[1] = state[1] + state[5];
+    state[13] = rotateLeft32By8(state[13] ^ state[1]);
+    state[9] = state[9] + state[13];
+    state[5] = rotateLeft32By7(state[5] ^ state[9]);
 
     // Quarter round on (2, 6, 10, 14)
-    state[2] = (state[2] + state[6]);
-    state[14] = _rotateLeft32(state[14] ^ state[2], 16);
-    state[10] = (state[10] + state[14]);
-    state[6] = _rotateLeft32(state[6] ^ state[10], 12);
-    state[2] = (state[2] + state[6]);
-    state[14] = _rotateLeft32(state[14] ^ state[2], 8);
-    state[10] = (state[10] + state[14]);
-    state[6] = _rotateLeft32(state[6] ^ state[10], 7);
+    state[2] = state[2] + state[6];
+    state[14] = rotateLeft32By16(state[14] ^ state[2]);
+    state[10] = state[10] + state[14];
+    state[6] = rotateLeft32By12(state[6] ^ state[10]);
+    state[2] = state[2] + state[6];
+    state[14] = rotateLeft32By8(state[14] ^ state[2]);
+    state[10] = state[10] + state[14];
+    state[6] = rotateLeft32By7(state[6] ^ state[10]);
 
     // Quarter round on (3, 7, 11, 15)
-    state[3] = (state[3] + state[7]);
-    state[15] = _rotateLeft32(state[15] ^ state[3], 16);
-    state[11] = (state[11] + state[15]);
-    state[7] = _rotateLeft32(state[7] ^ state[11], 12);
-    state[3] = (state[3] + state[7]);
-    state[15] = _rotateLeft32(state[15] ^ state[3], 8);
-    state[11] = (state[11] + state[15]);
-    state[7] = _rotateLeft32(state[7] ^ state[11], 7);
+    state[3] = state[3] + state[7];
+    state[15] = rotateLeft32By16(state[15] ^ state[3]);
+    state[11] = state[11] + state[15];
+    state[7] = rotateLeft32By12(state[7] ^ state[11]);
+    state[3] = state[3] + state[7];
+    state[15] = rotateLeft32By8(state[15] ^ state[3]);
+    state[11] = state[11] + state[15];
+    state[7] = rotateLeft32By7(state[7] ^ state[11]);
 
     // Diagonal rounds
 
     // Quarter round on (0, 5, 10, 15)
-    state[0] = (state[0] + state[5]);
-    state[15] = _rotateLeft32(state[15] ^ state[0], 16);
-    state[10] = (state[10] + state[15]);
-    state[5] = _rotateLeft32(state[5] ^ state[10], 12);
-    state[0] = (state[0] + state[5]);
-    state[15] = _rotateLeft32(state[15] ^ state[0], 8);
-    state[10] = (state[10] + state[15]);
-    state[5] = _rotateLeft32(state[5] ^ state[10], 7);
+    state[0] = state[0] + state[5];
+    state[15] = rotateLeft32By16(state[15] ^ state[0]);
+    state[10] = state[10] + state[15];
+    state[5] = rotateLeft32By12(state[5] ^ state[10]);
+    state[0] = state[0] + state[5];
+    state[15] = rotateLeft32By8(state[15] ^ state[0]);
+    state[10] = state[10] + state[15];
+    state[5] = rotateLeft32By7(state[5] ^ state[10]);
 
     // Quarter round on (1, 6, 11, 12)
-    state[1] = (state[1] + state[6]);
-    state[12] = _rotateLeft32(state[12] ^ state[1], 16);
-    state[11] = (state[11] + state[12]);
-    state[6] = _rotateLeft32(state[6] ^ state[11], 12);
-    state[1] = (state[1] + state[6]);
-    state[12] = _rotateLeft32(state[12] ^ state[1], 8);
-    state[11] = (state[11] + state[12]);
-    state[6] = _rotateLeft32(state[6] ^ state[11], 7);
+    state[1] = state[1] + state[6];
+    state[12] = rotateLeft32By16(state[12] ^ state[1]);
+    state[11] = state[11] + state[12];
+    state[6] = rotateLeft32By12(state[6] ^ state[11]);
+    state[1] = state[1] + state[6];
+    state[12] = rotateLeft32By8(state[12] ^ state[1]);
+    state[11] = state[11] + state[12];
+    state[6] = rotateLeft32By7(state[6] ^ state[11]);
 
     // Quarter round on (2, 7, 8, 13)
-    state[2] = (state[2] + state[7]);
-    state[13] = _rotateLeft32(state[13] ^ state[2], 16);
-    state[8] = (state[8] + state[13]);
-    state[7] = _rotateLeft32(state[7] ^ state[8], 12);
-    state[2] = (state[2] + state[7]);
-    state[13] = _rotateLeft32(state[13] ^ state[2], 8);
-    state[8] = (state[8] + state[13]);
-    state[7] = _rotateLeft32(state[7] ^ state[8], 7);
+    state[2] = state[2] + state[7];
+    state[13] = rotateLeft32By16(state[13] ^ state[2]);
+    state[8] = state[8] + state[13];
+    state[7] = rotateLeft32By12(state[7] ^ state[8]);
+    state[2] = state[2] + state[7];
+    state[13] = rotateLeft32By8(state[13] ^ state[2]);
+    state[8] = state[8] + state[13];
+    state[7] = rotateLeft32By7(state[7] ^ state[8]);
 
     // Quarter round on (3, 4, 9, 14)
-    state[3] = (state[3] + state[4]);
-    state[14] = _rotateLeft32(state[14] ^ state[3], 16);
-    state[9] = (state[9] + state[14]);
-    state[4] = _rotateLeft32(state[4] ^ state[9], 12);
-    state[3] = (state[3] + state[4]);
-    state[14] = _rotateLeft32(state[14] ^ state[3], 8);
-    state[9] = (state[9] + state[14]);
-    state[4] = _rotateLeft32(state[4] ^ state[9], 7);
+    state[3] = state[3] + state[4];
+    state[14] = rotateLeft32By16(state[14] ^ state[3]);
+    state[9] = state[9] + state[14];
+    state[4] = rotateLeft32By12(state[4] ^ state[9]);
+    state[3] = state[3] + state[4];
+    state[14] = rotateLeft32By8(state[14] ^ state[3]);
+    state[9] = state[9] + state[14];
+    state[4] = rotateLeft32By7(state[4] ^ state[9]);
   }
 }
 
@@ -123,21 +124,19 @@ void chacha20BlockRounds(Uint32List state) {
 /// - A 32-bit block count parameter, treated as a 32-bit little-endian integer.
 ///
 ///The output is 64 random-looking bytes.
-Uint32List chacha20Block(Uint32List key, int counter, Uint32List nonce) {
-  // final buffer = ByteBuffer (64);
-  final Uint32List state = Uint32List(16);
-
+Uint8List chacha20Block(Uint32List key, int counter, Uint32List nonce) {
   // Initialize the state with the constants, key, counter, and nonce
-  state[00] = 0x61707865;
-  state[01] = 0x3320646e;
-  state[02] = 0x79622d32;
-  state[03] = 0x6b206574;
-  state[04] = key[0];
-  state[05] = key[1];
-  state[06] = key[2];
-  state[07] = key[3];
-  state[08] = key[4];
-  state[09] = key[5];
+  final Uint32List state = Uint32List(16);
+  state[0] = 0x61707865;
+  state[1] = 0x3320646e;
+  state[2] = 0x79622d32;
+  state[3] = 0x6b206574;
+  state[4] = key[0];
+  state[5] = key[1];
+  state[6] = key[2];
+  state[7] = key[3];
+  state[8] = key[4];
+  state[9] = key[5];
   state[10] = key[6];
   state[11] = key[7];
   state[12] = counter;
@@ -156,10 +155,10 @@ Uint32List chacha20Block(Uint32List key, int counter, Uint32List nonce) {
 
   // Add the original state to the working state
   for (int i = 0; i < 16; i++) {
-    workingState[i] = (workingState[i] + state[i]);
+    workingState[i] += state[i];
   }
 
-  return workingState;
+  return workingState.buffer.asUint8List();
 }
 
 /// Encrypts or decrypts data using the ChaCha20 algorithm as specified in RFC 7539.
@@ -168,48 +167,29 @@ Uint8List chacha20(Uint8List key, Uint8List nonce, Uint8List data, [int counter 
   assert(nonce.length == 12, 'Invalid nonce');
 
   final int dataSize = data.lengthInBytes;
+  final Uint8List output = Uint8List(dataSize);
+
   final Uint32List key32Bit = Uint32List.view(key.buffer);
   final Uint32List nonce32Bit = Uint32List.view(nonce.buffer);
 
-  final ByteData outputBytes = ByteData(dataSize);
-
-  final Uint32List outputData = Uint32List.view(outputBytes.buffer);
-  final Uint32List inputData = Uint32List.view(data.buffer);
-
-  final int blocks = dataSize ~/ 64;
-  for (int i = 0; i < blocks; ++i) {
-    final Uint32List keyStream = chacha20Block(key32Bit, counter + i, nonce32Bit);
-    final int blockStartInts = i * 16;
-
-    for (int j = 0; j < 16; ++j) {
-      outputData[blockStartInts + j] = inputData[blockStartInts + j] ^ keyStream[j];
+  // Encrypt each full block
+  final int fullBlocks = dataSize ~/ 64;
+  for (int j = 0; j < fullBlocks; j++) {
+    final Uint8List keyStream = chacha20Block(key32Bit, counter + j, nonce32Bit);
+    for (int i = 0; i < 64; i++) {
+      output[j * 64 + i] = data[j * 64 + i] ^ keyStream[i];
     }
   }
 
-  if (dataSize % 64 != 0) {
-    final Uint32List keyStream = chacha20Block(key32Bit, counter + blocks, nonce32Bit);
-    final int blockStartInts = blocks * 16;
-    final int remainingBytes = dataSize % 64;
-    final int fullInts = remainingBytes ~/ 4; // Number of full 32-bit integers
-    final int extraBytes = remainingBytes % 4; // Number of extra bytes after full 32-bit integers
-
-    // Process full 32-bit integers
-    for (int i = 0; i < fullInts; i++) {
-      outputData[blockStartInts + i] = inputData[blockStartInts + i] ^ keyStream[i];
-    }
-
-    // Process any extra bytes
-    if (extraBytes > 0) {
-      final int startExtraByteIndex = blockStartInts * 4 + fullInts * 4;
-      for (int i = 0; i < extraBytes; i++) {
-        final int byteIndex = startExtraByteIndex + i;
-        outputBytes.setUint8(
-          byteIndex,
-          data[byteIndex] ^ keyStream.buffer.asUint8List()[fullInts * 4 + i],
-        );
-      }
+  // Handle any remaining partial block
+  final int remaining = dataSize % 64;
+  if (remaining != 0) {
+    final Uint8List keyStream = chacha20Block(key32Bit, counter + fullBlocks, nonce32Bit);
+    final int start = fullBlocks * 64;
+    for (int i = 0; i < remaining; i++) {
+      output[start + i] = data[start + i] ^ keyStream[i];
     }
   }
 
-  return outputBytes.buffer.asUint8List();
+  return output;
 }
