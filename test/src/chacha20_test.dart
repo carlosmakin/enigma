0fac632d6547aca914bd1e7a67c123acf31cc77e
Carlos Makin <carlos@makin.dev>
2024-01-21 19:32:51 -0700

    Update chacha20_test.dart

--- test/src/chacha20_test.dart
+++ test/src/chacha20_test.dart
@@ -2,102 +2,22 @@ import 'dart:typed_data';
 import 'package:enigma/src/chacha20.dart';
 import 'package:test/test.dart';
 
+typedef Chacha20TestVector = Map<String, dynamic>;
+
 void main() {
-  // test('ChaCha20 RFC 7539 2.2.1 Test Vector for the Quarter Round on the ChaCha State', () {
-  //   // Initial 32-bit unsigned integer list state
-  //   final Uint32List state = Uint32List.fromList(<int>[
-  //     0x879531e0,
-  //     0xc5ecf37d,
-  //     0x516461b1,
-  //     0xc9a62f8a,
-  //     0x44c20ef3,
-  //     0x3390af7f,
-  //     0xd9fc690b,
-  //     0x2a5f714c,
-  //     0x53372767,
-  //     0xb00a5631,
-  //     0x974c541a,
-  //     0x359e9963,
-  //     0x5c971061,
-  //     0x3d631689,
-  //     0x2098d9d6,
-  //     0x91dbd320
-  //   ]);
+  for (int i = 0; i < testVectors.length; i++) {
+    final Chacha20TestVector testVector = testVectors[i];
+    test('The ChaCha20 Block Functions Test Vector ${(i + 1)}', () {
+      final Uint32List key = parseBlockHexString(testVector['key']!).buffer.asUint32List();
+      final Uint32List nonce = parseBlockHexString(testVector['nonce']!).buffer.asUint32List();
+      final int counter = testVector['counter']! as int;
 
-  //   // Expected output after applying quarterRound(2,7,8,13)
-  //   const List<int> expectedState = <int>[
-  //     0x879531e0,
-  //     0xc5ecf37d,
-  //     0xbdb886dc,
-  //     0xc9a62f8a,
-  //     0x44c20ef3,
-  //     0x3390af7f,
-  //     0xd9fc690b,
-  //     0xcfacafd2,
-  //     0xe46bea80,
-  //     0xb00a5631,
-  //     0x974c541a,
-  //     0x359e9963,
-  //     0x5c971061,
-  //     0xccc07c79,
-  //     0x2098d9d6,
-  //     0x91dbd320
-  //   ];
+      final Uint8List keyStream = chacha20Block(key, counter, nonce);
+      final Uint8List expected = parseBlockHexString(testVector['keyStream']!);
 
-  //   // Running the ChaCha quarteRound on indices 2, 7, 8, 13
-  //   quarterRound(state, 2, 7, 8, 13);
-
-  //   // Asserting that the function's output matches the expected output for all elements
-  //   for (int i = 0; i < state.length; i++) {
-  //     expect(state[i], equals(expectedState[i]), reason: 'Mismatch at position $i');
-  //   }
-  // });
-
-  test('ChaCha20 RFC 7539 2.4.2. Example and Test Vector', () {
-    final Uint8List key = parseColonSeparatedHexString(keyHexString);
-    final Uint8List nonce = parseColonSeparatedHexString(nonceHexString);
-
-    final Uint8List input = parseBlockHexString(inputHexString);
-    final Uint8List output = chacha20(key, nonce, input);
-    final Uint8List expected = parseBlockHexString(outputhexString);
-
-    expect(output, equals(expected));
-  });
-}
-
-const String keyHexString = '''
-00:01:02:03:04:05:06:07:08:09:0a:0b:0c:0d:0e:0f:10:11:12:13:14:15:16:17:18:19:1a:1b:1c:1d:1e:1f
-''';
-const String nonceHexString = '''
-00:00:00:00:00:00:00:4a:00:00:00:00
-''';
-
-const String inputHexString = '''
-4c 61 64 69 65 73 20 61 6e 64 20 47 65 6e 74 6c
-65 6d 65 6e 20 6f 66 20 74 68 65 20 63 6c 61 73
-73 20 6f 66 20 27 39 39 3a 20 49 66 20 49 20 63
-6f 75 6c 64 20 6f 66 66 65 72 20 79 6f 75 20 6f
-6e 6c 79 20 6f 6e 65 20 74 69 70 20 66 6f 72 20
-74 68 65 20 66 75 74 75 72 65 2c 20 73 75 6e 73
-63 72 65 65 6e 20 77 6f 75 6c 64 20 62 65 20 69
-74 2e
-''';
-
-const String outputhexString = '''
-6e 2e 35 9a 25 68 f9 80 41 ba 07 28 dd 0d 69 81
-e9 7e 7a ec 1d 43 60 c2 0a 27 af cc fd 9f ae 0b
-f9 1b 65 c5 52 47 33 ab 8f 59 3d ab cd 62 b3 57
-16 39 d6 24 e6 51 52 ab 8f 53 0c 35 9f 08 61 d8
-07 ca 0d bf 50 0d 6a 61 56 a3 8e 08 8a 22 b6 5e
-52 bc 51 4d 16 cc f8 06 81 8c e9 1a b7 79 37 36
-5a f9 0b bf 74 a3 5b e6 b4 0b 8e ed f2 78 5e 42
-87 4d
-''';
-
-Uint8List parseColonSeparatedHexString(String hexString) {
-  final List<String> hexValues = hexString.replaceAll(' ', '').split(':');
-  final List<int> intList = hexValues.map((String hex) => int.parse(hex, radix: 16)).toList();
-  return Uint8List.fromList(intList);
+      expect(keyStream, equals(expected));
+    });
+  }
 }
 
 Uint8List parseBlockHexString(String hexString) {
@@ -110,3 +30,51 @@ Uint8List parseBlockHexString(String hexString) {
     hexBytes.map((String byte) => int.parse(byte, radix: 16)).toList(),
   );
 }
+
+const List<Chacha20TestVector> testVectors = <Chacha20TestVector>[
+  // Test Vector #1
+  <String, dynamic>{
+    'key':
+        '00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00',
+    'nonce': '00 00 00 00 00 00 00 00 00 00 00 00',
+    'counter': 0,
+    'keyStream':
+        '76 b8 e0 ad a0 f1 3d 90 40 5d 6a e5 53 86 bd 28 bd d2 19 b8 a0 8d ed 1a a8 36 ef cc 8b 77 0d c7 da 41 59 7c 51 57 48 8d 77 24 e0 3f b8 d8 4a 37 6a 43 b8 f4 15 18 a1 1c c3 87 b6 69 b2 ee 65 86',
+  },
+  // Test Vector #2
+  <String, dynamic>{
+    'key':
+        '00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00',
+    'nonce': '00 00 00 00 00 00 00 00 00 00 00 00',
+    'counter': 1,
+    'keyStream':
+        '9f 07 e7 be 55 51 38 7a 98 ba 97 7c 73 2d 08 0d cb 0f 29 a0 48 e3 65 69 12 c6 53 3e 32 ee 7a ed 29 b7 21 76 9c e6 4e 43 d5 71 33 b0 74 d8 39 d5 31 ed 1f 28 51 0a fb 45 ac e1 0a 1f 4b 79 4d 6f',
+  },
+  // Test Vector #3
+  <String, dynamic>{
+    'key':
+        '00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 01',
+    'nonce': '00 00 00 00 00 00 00 00 00 00 00 00',
+    'counter': 1,
+    'keyStream':
+        '3a eb 52 24 ec f8 49 92 9b 9d 82 8d b1 ce d4 dd 83 20 25 e8 01 8b 81 60 b8 22 84 f3 c9 49 aa 5a 8e ca 00 bb b4 a7 3b da d1 92 b5 c4 2f 73 f2 fd4e 27 36 44 c8 b3 61 25 a6 4a dd eb 00 6c 13 a0',
+  },
+  // Test Vector #4
+  <String, dynamic>{
+    'key':
+        '00 ff 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00',
+    'nonce': '00 00 00 00 00 00 00 00 00 00 00 00',
+    'counter': 2,
+    'keyStream':
+        '72 d5 4d fb f1 2e c4 4b 36 26 92 df 94 13 7f 32 8f ea 8d a7 39 90 26 5e c1 bb be a1 ae 9a f0 ca 13 b2 5a a2 6c b4 a6 48 cb 9b 9d 1b e6 5b 2c 09 24 a6 6c 54 d5 45 ec 1b 73 74 f4 87 2e 99 f0 96',
+  },
+  // Test Vector #5
+  <String, dynamic>{
+    'key':
+        '00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00',
+    'nonce': '00 00 00 00 00 00 00 00 00 00 00 02',
+    'counter': 0,
+    'keyStream':
+        'c2 c6 4d 37 8c d5 36 37 4a e2 04 b9 ef 93 3f cd 1a 8b 22 88 b3 df a4 96 72 ab 76 5b 54 ee 27 c7 8a 97 0e 0e 95 5c 14 f3 a8 8e 74 1b 97 c2 86 f7 5f 8f c2 99 e8 14 83 62 fa 19 8a 39 53 1b ed 6d',
+  },
+];
